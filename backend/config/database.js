b2bd98e351ccd0bd1a67ce22daaa18a0952b44d0
betortizPSG <betortiz_PSG@outlook.com.br>
2022-11-21 15:34:59 -0400

    Projeto ChatCripto
    Signed-off-by: betortizPSG <betortiz_PSG@outlook.com.br>

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const databaseConnect = () => {
+     mongoose.connect(process.env.DATABASE_URL,{
+          useNewUrlParser : true,
+          useUnifiedTopology : true
+     }).then(()=>{
+          console.log('Mongodb Database Connected')
+     }).catch(error=>{
+          console.log(error)
+     })
+}
+
+module.exports = databaseConnect;
