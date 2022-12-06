9edaeed6a536bf44ea98880626eb8e6a6f832367
InoueAkiraa <akiraij@hotmail.com>
2022-12-06 10:14:43 -0400

    css logout
    Signed-off-by: InoueAkiraa <akiraij@hotmail.com>

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,40 +1,42 @@
 import React, { useState } from "react";
 import { FaSignOutAlt } from "react-icons/fa";
 import { useDispatch } from "react-redux";
-import { userLogout } from '../store/actions/authAction'
+import { userLogout } from "../store/actions/authAction";
 
 const Logout = () => {
-
   const [hide, setHide] = useState(true);
   const dispatch = useDispatch();
 
   const logout = () => {
     dispatch(userLogout());
-  }
+  };
 
   return (
     <>
-    <div className="body">
-      <div onClick={logout} className='logout'>
-        <button><FaSignOutAlt /> Logout </button>
-      </div>
-
-      <div className={hide ? "theme_logout" : "theme_logout_show"}>
-        <h2>Dark Mode</h2>
-      </div>
-
-      <div>
-        <div className="on">
-          <label htmlFor="dark">ON</label>
-          <input type="radio" value="dark" name="theme" id="dark" />
+      <div className="body">
+        <div onClick={logout} className="logout">
+          <button>
+            <FaSignOutAlt /> Logout{" "}
+          </button>
         </div>
 
-        <div className="of">
-          <label htmlFor="white">OFF</label>
-          <input type="radio" value="white" name="theme" id="white" />
-        </div>
+        <div className="darkmode">
+          <div className={hide ? "theme_logout" : "theme_logout_show"}>
+            <h3>Dark Mode</h3>
+          </div>
         </div>
 
+        <div className="tema">
+          <div className="on">
+            <label htmlFor="dark">ON</label>
+            <input type="radio" value="dark" name="theme" id="dark" />
+          </div>
+
+          <div className="off">
+            <label htmlFor="white">OFF</label>
+            <input type="radio" value="white" name="theme" id="white" />
+          </div>
+        </div>
       </div>
     </>
   );
