345e22158d69b11385eb65d4903e6a6bff2bc5f7
DevEwertonOliveira <ewerton2007@gmail.com>
2022-12-15 09:21:42 -0400

    resolvido conflito socket

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,34 +1,29 @@
-import React, { useRef, useState, } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FaSignOutAlt } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { userLogout } from "../store/actions/authAction";
 import { themeSet } from "../store/actions/messengerAction";
 import { Form, FormGroup, Label, Input } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { io } from "socket.io-client";
+import { io } from "socket.io-client"
+
 
 const Logout = () => {
+  const socket = useRef()
   const [hide, setHide] = useState(true);
   const [state, setState] = useState(false);
-
-  const socket = useRef()
+  const { myInfo } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
-  socket.current = io("ws://localhost:8000");
+  useEffect(() => {
+    socket.current = io("ws://localhost:8000");
+  }, [])
+
+
   const logout = () => {
     dispatch(userLogout());
-    socket.current.emit("logout", myInfo.id);
+    socket.current.emit('logout', myInfo.id)
   };
-
-  const {
-    friends,
-    message,
-    mesageSendSuccess,
-    message_get_success,
-    themeMood,
-    new_user_add,
-  } = useSelector((state) => state.messenger);
-  const { myInfo } = useSelector((state) => state.auth);
 
   return (
     <>
